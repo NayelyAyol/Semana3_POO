51aa1eca1da6bc897a75b3c0a1c6d7642fc1ca45
POO <nayely.ayol@epn.edu.ec>
2025-05-01 07:53:21 -0500

    Nombre

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("semana3");
-        System.out.println("Nayely Ayol");
+        System.out.println("Nayely del Rocio Ayol Guanluisa");
         Scanner sc = new Scanner(System.in);
         Cocinero[] cocineros = new Cocinero[3];
 
